112589ed04999b1e6da950ba074b573a238a62a4
ChiefYuHan <mail.yuhanmeyer@gmail.com>
2024-11-23 18:18:51 +0100

    add player, movement, asteroids, collision, game over

--- main.py
+++ main.py
@@ -1,19 +1,50 @@
 import pygame
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 
 def main():
     print("Starting asteroids!")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    screen.fill(color="black")
+
+    clock = pygame.time.Clock()
+    dt = 0  # delta_time
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill(color="black")
+
+        for obj in updatable:
+            obj.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.detect_collision(player):
+                raise SystemExit("Game Over!")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
+        time_passed = clock.tick(
+            60
+        )  # pause game loop until 1/60 of a second has passed (60 loops per second)
+        dt = time_passed / 1000
 
 
 if __name__ == "__main__":
